69d20eb54294593d85c7cde17a3ca7d996fd989a
Jasvir Singh <jasvir.singh@grazitti.local>
2026-07-20 11:20:11 +0530

    merge support maerial work with master

--- themes/WushkaTheme/bookshelves.php
+++ themes/WushkaTheme/bookshelves.php
@@ -1156,22 +1156,19 @@ if (is_user_logged_in()) { ?>
             $plng_asset_label      = $a_level['planning_assets_label'];
             $phase_label           = $a_level['phase_label'];
 
-            //$display_sm = filter_input(INPUT_GET, 'sm_active', FILTER_VALIDATE_BOOLEAN);
+            get_template_part('template-parts/support-material/support-material-carousel', null, [
+                'o_term'                 => $o_term,
+                'daily_slideshow'        => $daily_slideshow,
+                'planning_assessments'   => $planning_assessments,
+                'ebooks'                 => $posts, 
+                'counter'                => $content_count,
+                'sh_asset_label'         => $sh_asset_label,
+                'plng_asset_label'       => $plng_asset_label,
+                'phase_label'            => $phase_label,
+                //'previous_carousel'    => $_SESSION['carousel-support-taxo-' . $o_term->term_taxonomy_id] ?? 0,
+                //'current_user'         => $current_user,
+            ]);
             
-            //if($display_sm):
-                get_template_part('template-parts/support-material/support-material-carousel', null, [
-                    'o_term'                 => $o_term,
-                    'daily_slideshow'        => $daily_slideshow,
-                    'planning_assessments'   => $planning_assessments,
-                    'ebooks'                 => $posts, 
-                    'counter'                => $content_count,
-                    'sh_asset_label'         => $sh_asset_label,
-                    'plng_asset_label'       => $plng_asset_label,
-                    'phase_label'            => $phase_label,
-                    //'previous_carousel'    => $_SESSION['carousel-support-taxo-' . $o_term->term_taxonomy_id] ?? 0,
-                    //'current_user'         => $current_user,
-                ]);
-            //endif;
     ?>
             <div class="shelf-wrapper<?php echo $blocked ?>" <?= $main_content; ?>>
                 <div class="container-fluid">
