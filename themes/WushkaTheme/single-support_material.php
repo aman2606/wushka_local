b969fceeb4871f649dd06ad99c6f231fbadc7f6e
Jasvir Singh <jasvir.singh@grazitti.local>
2026-07-27 14:31:48 +0530

    support maerial client feedback deploy

--- themes/WushkaTheme/single-support_material.php
+++ themes/WushkaTheme/single-support_material.php
@@ -1,16 +1,18 @@
 <?php
 /**
  * Support material viewer template.
- *
  */
+$sm_type = isset( $_GET['sm_type'] ) ? sanitize_text_field( wp_unslash( $_GET['sm_type'] ) ) : '';
+$viewer  = isset( $_GET['viewer'] ) ? sanitize_text_field( wp_unslash( $_GET['viewer'] ) ) : '';
+$id      = isset( $_GET['id'] ) ? sanitize_text_field( wp_unslash( $_GET['id'] ) ) : '';
 
-//if ( isset( $_GET['sm_type'] ) && isset( $_GET['id'] ) && ! empty( $_GET['id'] ) ) {
-if ( in_array( $_GET['sm_type'] ?? '', ['slideshow'], true ) && !empty( $_GET['id'] ) ) {
-    get_template_part('template-parts/support-material/slideshow-and-lesson-plans', null, []); 
-} elseif ( isset( $_GET['sm_type'] ) && in_array( $_GET['sm_type'] ?? '', ['assessment', 'lesson_plan'], true ) ) {
-    get_template_part('template-parts/support-material/planning-and-assessments', null, []);
+$is_slideshow  = ( $sm_type === 'slideshow' && $id !== '' );
+$is_assessment = in_array( $sm_type, [ 'assessment', 'lesson_plan' ], true );
+
+if ( $is_slideshow && $viewer === 'ppt' ) {
+    get_template_part( 'template-parts/support-material/ppt-viewer', null, [] );
+}  elseif ( $is_assessment || $is_slideshow) {
+    get_template_part( 'template-parts/support-material/fliphtml-viewer', null, [] );
 } else {
     echo esc_html__( 'No slideshow or assessment specified.', 'your-textdomain' );
 }
-
-/* ----- END OF FILE ----- */
